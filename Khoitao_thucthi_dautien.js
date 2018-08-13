6a4726a1461e7b8f28e9311ef482d0cb2c09c5dd
dinhpv38 <manroid38@gmail.com>
2018-08-13 11:32:22 +0700

    test

--- Khoitao_thucthi_dautien.js
+++ Khoitao_thucthi_dautien.js
@@ -1,9 +1,11 @@
 
 
-(function () {
+(function (lastName) {
     var fisrtName = "Hoang";
     console.log(fisrtName);
- }())
+    console.log(lastName);
+
+ }("Hung"))
  
 var LastName = "Long";
 console.log(LastName);
