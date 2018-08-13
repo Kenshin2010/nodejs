aa94c29140a70b179b09341b747887e39f57b8ce
dinhpv38 <manroid38@gmail.com>
2018-08-13 16:49:21 +0700

    exports va module exports

---
+++ exports_module.exports/hello1.js
@@ -0,0 +1,6 @@
+exports = function () {
+    console.log("Hello nodejs 1 !")
+}
+
+console.log(exports);
+console.log(module.exports);
---
+++ exports_module.exports/hello2.js
@@ -0,0 +1,6 @@
+exports.sayHello = function () {
+    console.log("Hello nodejs 2 !")
+}
+
+console.log(exports);
+console.log(module.exports);
