c4b43c3f172bb08d26f3c89f7830de442db2f95e
Manroid <manroid38@gmail.com>
2018-08-15 10:06:27 +0700

    update

---
+++ es6_template_string/app.js
@@ -0,0 +1,17 @@
+var person = {
+    name : "Hoa",
+    age : 20
+}
+
+
+console.log(`Toi la ${person.name} va toi ${person.age}
+
+
+
+
+
+ ===================== xuong dong ========================
+
+
+
+`);
