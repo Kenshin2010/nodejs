715fc71ae35fded5f4b336bebd2eebc05c82a1d7
dinhpv38 <manroid38@gmail.com>
2018-08-20 15:02:02 +0700

    update

---
+++ tra_ve_html_template_don_gian/app2.js
@@ -0,0 +1,8 @@
+var http = require("http");
+var fs = require("fs");
+
+http.createServer(function (req,res) {
+    res.writeHead(200,{'Content-Type' : 'text/html'});
+    fs.createReadStream(__dirname + "/index.html").pipe(res);
+    // res.end("Hello Nodejs for Android");
+}).listen(8080,"127.0.0.1")
