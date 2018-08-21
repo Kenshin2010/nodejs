399a59aada6fef1849c1a5e750257c448129a4e3
Doan Pham <phamdoanmanu38@gmail.com>
2018-08-21 15:41:42 +0700

    update

--- service/web_service/express/app.js
+++ service/web_service/express/app.js
@@ -1,10 +1,15 @@
 var express = require("express");
 var cookieParser = require("cookie-parser");
+var bodyParser = require('body-parser');
 
 var app = express();
 
 
 var port = 3000;
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+// create application/json parser
+var jsonParser = bodyParser.json()
 
 app.use("/assets", express.static(__dirname + "/public"));
 app.use(cookieParser());
@@ -33,7 +38,7 @@ app.use("/", function (req, res, next) {
 
 
 app.get("/", function (req, res) {
-   res.render("index");
+    res.render("index");
 })
 
 app.get("/api", function (req, res) {
@@ -52,8 +57,20 @@ app.get("/user/:id", function (req, res) {
     // res.send(`<h1>user : ${req.params.id}`);
 
     //render file ejs test 
-    res.render("user", {ID : req.params.id})
-}) 
+    res.render("user", { ID: req.params.id, queryString: req.query.qstr });
+})
+
+app.post("/login", urlencodedParser, function (req, res) {
+    res.send("Welcome : " + req.body.username);
+    console.log(req.body.username);
+    console.log(req.body.password);
+})
+
+app.post("/loginjson", jsonParser, function (req,res) {
+    res.send("OK");
+    console.log(req.body.firstName);
+    console.log(req.body.lastName);
+})
 
 app.listen(port, function () {
     console.log("server listening on PORT " + port);
