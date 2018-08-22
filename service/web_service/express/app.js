5fad60e0b5a1ba13dc5a8cab9e1cdbc2546756cc
Doan Pham <phamdoanmanu38@gmail.com>
2018-08-22 14:40:47 +0700

    update

--- service/web_service/express/app.js
+++ service/web_service/express/app.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var cookieParser = require("cookie-parser");
+
+var mongoose = require("mongoose");
+// var mysql = require("mysql");
 // var bodyParser = require('body-parser');
 
 var apiController = require("./controllers/apiController");
@@ -14,101 +17,101 @@ var port = 3000;
 // // create application/json parser
 // var jsonParser = bodyParser.json()
 
+
+
+mongoose.connect("mongodb://test:android123@ds229732.mlab.com:29732/manroiddb")
+var Schema = mongoose.Schema;
+var personSchema = new Schema({
+    fistName: String,
+    lastName: String,
+    address: String
+
+})
+
+var Person = mongoose.model("Person",personSchema);
+
+
+
+
+
 app.use("/assets", express.static(__dirname + "/public"));
 app.use(cookieParser());
 app.set("view engine", "ejs");
 
 
-//custom middleware
-
+//custom middleware base
 app.use("/", function (req, res, next) {
     console.log("Request Url : " + req.url);
     req.requestTime = new Date();
+
+
+
+    //create a obj
+    var hoa = Person({
+        fistName : "Hoa",
+        lastName : "Mai",
+        address : "HA NOI"
+    });
+
+    hoa.save(function (err) {
+        if(err) throw err;
+        console.log("Hoa created ... !");
+    });
+
+    var phuong = Person({
+        fistName : "PHuong",
+        lastName : "Yen",
+        address : "HA NOI"
+    });
+
+    phuong.save(function (err) {
+        if(err) throw err;
+        console.log("PHuong created ... !");
+    });
+
     next();
 })
 
 
+//custom middleware for sql test xampp
+// app.use("/", function (req, res, next) {
+//     console.log("Request Url : " + req.url);
+//     req.requestTime = new Date();
+
+//     var connecttion = mysql.createConnection({
+//         host: "localhost",
+//         user: "",
+//         password: "",
+//         database: "test"
+//     });
+
+//     connecttion.connect();
+
+//     // connecttion.query(`SELECT * FROM tbl_sinhvien`, function (err,rows)
+//     connecttion.query("SELECT * FROM tbl_sinhvien WHERE MASV = 'SV001'", function (err, rows) {
+//         if (err) {
+//             throw err;
+//         }
+
+//         console.log(rows[0].TEN);
+//     });
+
+//     connecttion.end();
+
+//     next();
+// })
+
+
 apiController(app);
 homeController(app);
-// other 
 
-// app.get("/", function (req, res) {
-//     console.log("cookieParser", req.cookies);
-//     res.send(`
-//     <link href="/assets/style.css" rel="stylesheet" type="text/css">
-//     <h1> Hello express !!! </h1>
-//     <p> Request time :  ${req.requestTime}}</p>
-//     `);
-// })
-
-
-// app.get("/", function (req, res) {
-//     res.render("index");
-// })
-
-// app.get("/api/user/:id", function (req, res) {
-//     //get data from database
-//     // res.render("user", { ID: req.params.id, queryString: req.query.qstr });
-//     var result = {
-//         fisrtName: "Mai",
-//         lastName: "Hoa"
-//     }
-//     res.json(result);
-// })
-
-// app.post("/api/user", jsonParser, function (req, res) {
-//     //create new and save to the db
-
-// })
-
-// app.put("/api/user", jsonParser, function (req, res) {
-//     //update new and save to the db
-
-// })
-
-
-// app.delete("/api/user/:id", function (req, res) {
-//     //delete user from db
-
-// })
-
-//===============test
-// app.get("/api/", function (req, res) {
-//     res.json({
-//         fisrtName: "Mai",
-//         lastName: "Hoa"
-//     });
-// })
-
-/**
- * product : id
- * user : id
- */
-
-//===============test
-// app.get("/user/:id", function (req, res) {
-// res.cookie("userName", req.param.id);
-// res.send(`<h1>user : ${req.params.id}`);
-
-//render file ejs test 
-// res.render("user", { ID: req.params.id, queryString: req.query.qstr });
-// })
-
-// app.post("/login", urlencodedParser, function (req, res) {
-//     res.send("Welcome : " + req.body.username);
-//     console.log(req.body.username);
-//     console.log(req.body.password);
-// })
-
-// app.post("/loginjson", jsonParser, function (req, res) {
-//     res.send("OK");
-//     console.log(req.body.firstName);
-//     console.log(req.body.lastName);
-// })
 
 app.listen(port, function () {
     console.log("server listening on PORT " + port);
 });
+
+
+
 
 
 
